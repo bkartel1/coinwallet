0750f23f02b3e29d288d41fcc4bc035c9f133083
ftufek <furkan.dev@gmail.com>
2014-02-18 16:50:47 -0500

    Implemented some parts of the send bitcoins UI.

--- CoinWallet/src/main/java/com/teambitcoin/coinwallet/SendBitcoins.java
+++ CoinWallet/src/main/java/com/teambitcoin/coinwallet/SendBitcoins.java
@@ -27,6 +27,10 @@ public class SendBitcoins extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		return false;
+	}
+	
+	public void sendBitcoins(View view){
+		Toast.makeText(this, "SENDING BITCOINS!!!", Toast.LENGTH_SHORT).show();
 	}
 }
