663fed5b05ee27f0874cfad20f07b90ec868082c
Aqsa Ahmed <aqsa.ahmed90@gmail.com>
2014-02-23 08:38:52 -0500

    Test cases
    A shell for logout and sending Bitcoins test cases.

---
+++ CoinWallet-tests/src/main/java/com/teambitcoin/coinwallet/tests/SendBitcoinTest.java
@@ -0,0 +1,16 @@
+package com.teambitcoin.coinwallet.tests;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.teambitcoin.coinwallet.*;
+import com.teambitcoin.coinwallet.models.User;
+
+public class SendBitcoinTest {
+	
+	@Test
+	public void test1(){
+		
+	}
+}
